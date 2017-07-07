52794c9244367ca3fb8dd0bde2e70b4d5fa60299
Kristopher Johnson <kris@kristopherjohnson.net>
2017-07-07 19:45:17 -0400

    Use SITESUBTITLE variable in theme

--- undefinedvalue/pelicanconf.py
+++ undefinedvalue/pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Kristopher Johnson'
 SITENAME = 'Undefined Value'
-SITESLOGAN = "Kris Johnson's Blog"
+SITESUBTITLE = "Kris Johnson's Blog"
 SITEURL = ''
 
 PATH = 'content'
