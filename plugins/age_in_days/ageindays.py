60b8458002fe90bd35518e0dfee6266d69bf8619
Kristopher Johnson <kris@kristopherjohnson.net>
2017-07-22 11:47:36 -0400

    Remove unnecessary import

--- plugins/age_in_days/ageindays.py
+++ plugins/age_in_days/ageindays.py
@@ -1,12 +1,7 @@
-"""
-age_in_days is a Jinja2 filter that given a datetime
-returns the number of days since that time.
-"""
-
-from datetime import datetime, timedelta
+from datetime import datetime
 
 def age_in_days(dt):
-    """Return a number of days between now and a specified datetime."""
+    """Return a number of days between now and an earlier datetime."""
     now = datetime.now(dt.tzinfo)
     delta = now - dt
     return delta.days
