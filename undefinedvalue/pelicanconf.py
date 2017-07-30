42d196c6cda1ccb6b3b2d9bfbb2f9594c1672c17
Kristopher Johnson <kris@kristopherjohnson.net>
2017-07-30 12:06:05 -0400

    Restore all templates

--- undefinedvalue/pelicanconf.py
+++ undefinedvalue/pelicanconf.py
@@ -60,7 +60,7 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
-DIRECT_TEMPLATES = ['index', 'tags', 'archives', 'search']
+DIRECT_TEMPLATES = ['index', 'tags', 'categories', 'authors', 'archives', 'search']
 
 PLUGIN_PATHS = ['../plugins', '../pelican-plugins']
 PLUGINS = ['age_in_days', 'pelican_alias', 'neighbors', 'sitemap']
